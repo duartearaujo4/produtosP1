8de11019e935805fc356291a3ab3e111ea316108
duartearaujo4 <120731063+duartearaujo4@users.noreply.github.com>
2023-05-11 14:20:58 +0100

    Update trabalho 1 P1.cpp

--- trabalho 1 P1.cpp
+++ trabalho 1 P1.cpp
@@ -3,3 +3,39 @@
 #include <string.h>
 
 #define MAX_PRODUTOS 250
+
+typedef struct  Produtos {
+	char nome [30];
+	char sku [15];
+	int quantidade;
+	float preço;
+	int id;
+	int categoria;
+};
+
+typedef struct Categoria {
+	char nome [30];
+	int id;
+};
+
+typedef struct Clientes {
+	char nome [30];
+	int NIF [9];
+	char telefone [9];
+};
+
+
+
+
+printf("Digite o nome do novo produto:");
+scanf("%s");
+printf("Digite o SKU do novo produto:");
+scanf("%s");
+printf("Digite a quantidade(stock) do novo produto:");
+scanf("%d");
+printf("Digite o preço do novo produto:");
+scanf("&f");
+printf("Selecione a categoria do novo produto:");
+scanf
+printf("Novo produto adicionado!");
+
