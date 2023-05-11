4e63565526f2eb50bf4e74f1359758aec7dc54d7
duartearaujo4 <120731063+duartearaujo4@users.noreply.github.com>
2023-05-11 16:24:14 +0100

    Update trabalho 1 P1.cpp

--- trabalho 1 P1.cpp
+++ trabalho 1 P1.cpp
@@ -6,38 +6,72 @@
 
 #define MAX_PRODUTOS 250
 
-typedef struct  Produtos {
+typedef struct  produtos {
 	char nome [30];
 	char sku [15];
 	int quantidade;
 	float preço;
 	int id;
 	int categoria;
-};
+}caracteristicas;
 
-typedef struct Categoria {
-	char nome [30];
-	int id;
-};
+typedef struct catg {
+	INFO node;
+	struct catg* next;
+	struct elem* prev;
+}categoria;
 
-typedef struct Clientes {
-	char nome [30];
-	int NIF [9];
-	char telefone [9];
-};
+categoria* criar_categoria(INFO new_info){
+	categoria* nova = (categoria*) calloc(1, sizeof(categoria));
+	nova ->node = new_info;
+	nova ->next =NULL;
+	nova ->prev =NULL;
+	return novo;
+}
+categoria* inserir_categoria_inicio(categoria* lista, INFO new_info){
+	categoria* nova = criar_categoria(new_info);
+	if(lista!= NULL){
+		lista->prev = nova;
+	}
+	nova->next = lista;
+	nova->prev = NULL;
+	return nova;
+}
+void adicionarproduto
+	if (contadorprodutos > MAX_PRODUTOS ) {
+		printf("A loja nao tem capacidade para mais produtos!");
+	}
+
+void removerproduto
+    if (contadorprodutos == 0) {
+        printf("Nenhum produto cadastrado.\n");
+    }
 
 
-
-
+Produto novoproduto;
 printf("Digite o nome do novo produto:");
-scanf("%s");
+scanf("%s, novoproduto.nome");
 printf("Digite o SKU do novo produto:");
-scanf("%s");
+scanf("%s, novoproduto.sku");
 printf("Digite a quantidade(stock) do novo produto:");
-scanf("%d");
+scanf("%d, novoproduto.quantidade");
 printf("Digite o preço do novo produto:");
-scanf("&f");
+scanf("&f, novoproduto.preço");
 printf("Selecione a categoria do novo produto:");
-scanf
 printf("Novo produto adicionado!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
